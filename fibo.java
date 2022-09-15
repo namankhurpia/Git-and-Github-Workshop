dc36040e3bf742652c3ef28a558dea99d31ef114
elrodrigues <elvis@elrodrigues.com>
2022-09-15 18:05:17 -0400

    nice loop

--- fibo.java
+++ fibo.java
@@ -3,7 +3,7 @@ class fibonacci {
     {
         if (n <= 1)
             return n;
-        return fib(n - 1) + fib(n - 2);
+        return fib(n + 1) + fib(n - 2);
     }
  
     public static void main(String args[])
